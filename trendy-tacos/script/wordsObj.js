731b8b00edea8816648b28336df555a35842949b
Vivek Arora <vivek.arora2024@gmail.com>
2021-01-14 17:59:30 +0530

    added more words in file

--- trendy-tacos/script/wordsObj.js
+++ trendy-tacos/script/wordsObj.js
@@ -2,4 +2,11 @@ export let items = {
   word1: ["girl", "boy", "golyrbi", "hint1"],
   word2: ["old", "young", "gondylou", "hint2"],
   word3: ["up", "down", "wpnuOd", "hint3"],
+  word4: ["CorelDraw","Inkscape","CoInrekslDcarapew","vector drawing program"],
+  word5: ["VisioProfessional","Dia","DVPfiaessioisional","creates diagrams—for flowcharts, org charts, and more"],
+  word6: ["MicrosoftOffice","OpenOffice","oftOficesorciMnepOOeicff"," Office Productivity"],
+  word7: ["RoboForm","KeePass","mssraoPFeoebKoR"," open source password manager"],
+  word8: ["AutoCAD","FreeCAD","DDAACCeoetruFA","drawing software"],
+  word9: ["Evernote","NixNote","eettrevEixNoNo","help you take notes and stay organized"],
+  word10: ["CCleaner","BleachBit","itchleaBnaeerCBCl","guards your privacy and frees disk space"],
 };
